940104e46d0b00f7736d0a5f7682b84af21408d4
Fernando Vargas Filho <fernandov.filho@gmail.com>
2021-01-02 02:17:29 -0300

    feat: category resolver

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from "apollo-server";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
+import CategoryResolver from "./graphql/category/CategoryResolver";
 import "./utils/connetion";
 
 (async () => {
   const schema = await buildSchema({
-    resolvers: [],
+    resolvers: [CategoryResolver],
   });
 
   const server = new ApolloServer({ schema });
